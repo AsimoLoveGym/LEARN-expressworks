59646430bc4537a1e7288c9aaf99431511a2f982
AsimoLoveGym <xiazhuangz@gmail.com>
2016-11-20 02:39:45 -0600

    [TASK 5]Really confusing task

--- program.js
+++ program.js
@@ -41,16 +41,42 @@
 
 // TASK 4
 
+// var express = require("express");
+// var bodyparser = require('body-parser');
+// var app = express();
+// var port = process.argv[2];
+//
+// app.use(bodyparser.urlencoded({extended: false}));
+//
+// app.post('/form', function(request, response){
+//   // console.log();
+//   response.end(request.body.str.split('').reverse().join(''));
+// })
+//
+// app.listen(port);
+
+// TASK 5
+
 var express = require("express");
-var bodyparser = require('body-parser');
 var app = express();
 var port = process.argv[2];
+var contentPath = process.argv[3];
+var options = {
+  extensions: 'html'
+}
 
-app.use(bodyparser.urlencoded({extended: false}));
+// console.log(contentPath);
 
-app.post('/form', function(request, response){
-  // console.log();
-  response.end(request.body.str.split('').reverse().join(''));
-})
+app.use(require('stylus').middleware(contentPath));
+app.use(express.static(contentPath));
 
 app.listen(port);
+
+// var express = require('express')
+// var app = express()
+//
+// app.use(require('stylus').middleware(process.argv[3]+ "/*.styl" ));
+// // app.use(express.static(process.argv[3]));
+//
+//
+// app.listen(process.argv[2])
